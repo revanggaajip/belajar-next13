cb8ff23f68910f9ec540e58132c0766623f134c2
Revangga Aji Pratama <revanggaajip2@gmail.com>
2023-05-09 19:58:28 +0700

    Implements fetch data to table

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -15,9 +15,28 @@ export default async function ProductList() {
     return (
         <div>
             <h1>Product List</h1>
-            {products.map((product, index) => (
-                <p key={product.id}>{product.title}</p>
-            ))}
+            <div className="mt-4 p-6">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Product Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product, index) => (
+                            <tr key={product.id}>
+                                <td>{index + 1}</td>
+                                <td>{product.title}</td>
+                                <td>{product.price}</td>
+                                <td></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
